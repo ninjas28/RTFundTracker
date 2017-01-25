a4d64aec6a0c0ac6b3736d19bbee17b2ec87b4eb
System <none>
2017-01-25 03:52:23 +0000

    Updated with live quotes

--- server.js
+++ server.js
@@ -4,7 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
-var finance = require('yahoo-finance-data');
+var finance = require('yahoo-finance');
+var _ = require('lodash');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -13,27 +14,17 @@ var finance = require('yahoo-finance-data');
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/a/:lol", function (request, response) {
+app.get("/live/:quote", function (request, response) {
+  var SYMBOL = request.params['quote'];
   
-  response.send(request.params['lol']);
+  finance.snapshot({
+    fields: ['s', 'n', 'd1', 'l1', 'y', 'r'] ,
+    symbol: SYMBOL
+  }, function (err, snapshot) {
+    if (err) { throw err; }
+    response.send(JSON.stringify(snapshot, null, 2));
+  });
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
